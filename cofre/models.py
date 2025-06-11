682c72ff7f0b2ab8f47fe79324f477b407627e6b
Clécio <teixeira.clecio@hotmail.com>
2025-06-10 23:22:50 -0300

    corrigindo o models.py

--- cofre/models.py
+++ cofre/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Password(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='password_items')
     title = models.CharField(max_length=100)
     password = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -11,10 +11,11 @@ class Password(models.Model):
         return f"{self.user.username} - {self.title}"
 
 class Note(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='note_items')
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return f"{self.user.username} - {self.title}"
+
